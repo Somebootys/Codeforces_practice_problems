11fe71c87ad15e088354abb41f0ac9a5e54612f3
Somebootys <60421381+Somebootys@users.noreply.github.com>
2022-09-19 14:40:06 +0800

    Update watermelon_solution.rs
    code cleaner

--- watermelon_solution.rs
+++ watermelon_solution.rs
@@ -1,22 +1,21 @@
 #![allow(unused)]
+#![allow(unused)]
 
-fn even_uneven(x: u8)->String {
-    if x%2 == 0 {return "YES".to_string()}
-    
-    return "NO".to_string()
-    
+fn is_even(x: u8) -> bool {
+    x % 2 == 0
 }
 
-    
-    
-    fn main() {  
+fn main() {
     let mut i: u8 = 1;
-    loop{
-        
-        
-        println!("Can the value: {}\n be divided by two evenly?   {:?}",i, even_uneven(i));
-        i+=1;
-        if i ==10{break;}
+    loop {
+        if is_even(i) {
+            println!("YES");
+        } else {
+            println!("NO");
         }
-            
+        i += 1;
+        if i > 10{
+            break;
+        }
     }
+}
